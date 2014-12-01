58402da082fb172c3f80db602a56fd451d29d4c8
Ragaeeb Haq <rhaq@blackberry.com>
2014-12-01 17:17:08 -0500

    Additional plumbing.

--- src/ApplicationUI.cpp
+++ src/ApplicationUI.cpp
@@ -14,7 +14,7 @@ namespace messagetemplates {
 using namespace bb::cascades;
 using namespace canadainc;
 
-ApplicationUI::ApplicationUI(bb::cascades::Application *app) : QObject(app), m_cover("Cover.qml")
+ApplicationUI::ApplicationUI(bb::cascades::Application *app) : QObject(app), m_cover("Cover.qml"), m_root(NULL)
 {
 	INIT_SETTING("onlyInbound", 1);
     INIT_SETTING(CARD_KEY, true);
@@ -22,70 +22,78 @@ ApplicationUI::ApplicationUI(bb::cascades::Application *app) : QObject(app), m_c
 
 	switch ( m_invokeManager.startupMode() )
 	{
+    case ApplicationStartupMode::LaunchApplication:
+        LogMonitor::create(UI_KEY, UI_LOG_FILE, this);
+        init("main.qml");
+        break;
+
 	case ApplicationStartupMode::InvokeCard:
 		connect( &m_invokeManager, SIGNAL( invoked(bb::system::InvokeRequest const&) ), this, SLOT( invoked(bb::system::InvokeRequest const&) ) );
 		connect( &m_invokeManager, SIGNAL( childCardDone(bb::system::CardDoneMessage const&) ), this, SLOT( childCardDone(bb::system::CardDoneMessage const&) ) );
 		break;
 
+    case ApplicationStartupMode::InvokeApplication:
+        LogMonitor::create(UI_KEY, UI_LOG_FILE, this);
+        connect( &m_invokeManager, SIGNAL( invoked(bb::system::InvokeRequest const&) ), this, SLOT( invoked(bb::system::InvokeRequest const&) ) );
+        break;
+
 	default:
-		initRoot();
+	    break;
 		break;
 	}
 }
 
 
-QObject* ApplicationUI::initRoot(QString const& qmlSource, bool invoked)
+void ApplicationUI::invoked(bb::system::InvokeRequest const& request)
 {
-	QmlDocument* qml = QmlDocument::create("asset:///"+qmlSource).parent(this);
-    qml->setContextProperty("app", this);
-    qml->setContextProperty("persist", &m_persistance);
-    qml->setContextProperty("localizer", &m_locale);
+    LOGGER( request.action() << request.target() << request.mimeType() << request.metadata() << request.uri().toString() << QString( request.data() ) );
 
-    AbstractPane* root;
+    QMap<QString,QString> targetToQML;
 
-    if (invoked) {
-    	Page* r = qml->createRootObject<Page>();
-    	NavigationPane* np = NavigationPane::create().backButtons(true);
-    	np->push(r);
-    	Application::instance()->setScene(np);
+    QString qml = targetToQML.value(target);
 
-    	root = r;
-    } else {
-        root = qml->createRootObject<AbstractPane>();
-        Application::instance()->setScene(root);
+    if ( qml.isNull() ) {
+        qml = "TemplatesPage.qml";
     }
 
-	connect( this, SIGNAL( initialize() ), this, SLOT( init() ), Qt::QueuedConnection ); // async startup
-	emit initialize();
+    init(qml);
+
+    m_request = request;
+}
+
+
+QObject* ApplicationUI::initRoot(QString const& qmlSource)
+{
+    QMap<QString, QObject*> context;
+    context.insert("localizer", &m_locale);
+
+    LOGGER("Instantiate" << qmlDoc);
+    m_root = CardUtils::initAppropriate(qmlDoc, context, this);
+    emit initialize();
 
 	return root;
 }
 
 
-void ApplicationUI::invoked(bb::system::InvokeRequest const& request)
+void ApplicationUI::lazyInit()
 {
-	QStringList tokens = request.uri().toString().split(":");
-    LOGGER("========= INVOKED DATA" << tokens);
-
-    if ( tokens.size() > 3 ) {
-    	QObject* root = initRoot("TemplatesPage.qml", true);
-    	qint64 accountId = tokens[2].toLongLong();
-    	QString messageId = tokens[3];
-
-    	QVariantMap map;
-    	map["id"] = messageId;
-
-    	root->setProperty("accountId", accountId);
-    	root->setProperty("message", map);
-    } else {
-    	initRoot();
+    if ( !Persistance::hasEmailSmsAccess() ) {
+        m_persist.showToast( tr("Warning: It seems like the app does not have access to your Email/SMS messages Folder. This permission is needed for the app to access the SMS and email services it needs to validatebe able to reply to them via templates. If you leave this permission off, some features may not work properly. Select OK to launch the Application Permissions screen where you can turn these settings on.") );
     }
-}
 
+    QStringList tokens = m_request.uri().toString().split(":");
 
-void ApplicationUI::init()
-{
-	InvocationUtils::validateEmailSMSAccess( tr("Warning: It seems like the app does not have access to your Email/SMS messages Folder. This permission is needed for the app to access the SMS and email services it needs to validatebe able to reply to them via templates. If you leave this permission off, some features may not work properly. Select OK to launch the Application Permissions screen where you can turn these settings on.") );
+    if ( tokens.size() > 3 )
+    {
+        qint64 accountId = tokens[2].toLongLong();
+        QString messageId = tokens[3];
+
+        QVariantMap map;
+        map["id"] = messageId;
+
+        m_root->setProperty("accountId", accountId);
+        m_root->setProperty("message", map);
+    }
 }
 
 
@@ -99,7 +107,7 @@ void ApplicationUI::loadAccounts()
 
 void ApplicationUI::loadMessages(qint64 accountId)
 {
-	LOGGER("===== Load messages for" << accountId);
+	LOGGER("Load messages for" << accountId);
 	MessageImporter* ai = new MessageImporter( accountId, m_persistance.getValueFor("onlyInbound").toInt() == 1 );
 	connect( ai, SIGNAL( importCompleted(QVariantList const&) ), this, SIGNAL( messagesImported(QVariantList const&) ) );
 	connect( ai, SIGNAL( progress(int, int) ), this, SIGNAL( loadProgress(int, int) ) );
@@ -122,7 +130,6 @@ void ApplicationUI::processReply(qint64 accountId, QVariantMap const& message, Q
 void ApplicationUI::childCardDone(bb::system::CardDoneMessage const& message)
 {
 	m_invokeManager.sendCardDone(message);
-	//exit(0);
 }
 
 
